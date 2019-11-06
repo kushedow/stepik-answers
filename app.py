b3f7a8c90779cf02d0d2a1dcd86fa3efd7663259
kushedow <Polemika2020>
2019-11-06 12:00:30 +0300

    tryin to fix

--- app.py
+++ app.py
@@ -1,14 +1,45 @@
-from flask import Flask
+from flask import *
 import requests
 
-app = Flask(__name__)     
+app = Flask(__name__)
+app.config['DEBUG'] = True
 
-@app.route('/')          
+sheetdb_id = "ropn4xlz2e32m"
 
-def hello():
+@app.route('/')
 
-  r = requests.get( < url >)
+def main():
 
-  return 'Hello, World!'   
+  output = render_template("main.html")  # рендерим шаблон
+  return output  # возвращаем то, что отрендерилось
 
-app.run('0.0.0.0',8000)     
+@app.route('/solution/')
+
+def solution():
+
+  stepid = request.values.get("stepid")
+
+  if len(stepid)<1:
+    return render_template("404.html")  # рендерим шаблон
+
+  query = "https://sheetdb.io/api/v1/"+sheetdb_id+"/search?stepid="+stepid
+
+  session = requests.Session()
+  session.trust_env = False
+  r = session.get(query)
+
+  data = r.json()
+
+  if len(data)>0:
+
+    solution = data[0]
+
+    output = render_template("solution.html",solution=solution)  # рендерим шаблон
+    return output  # возвращаем то, что отрендерилось
+
+  else:
+
+    return render_template("404.html")  # рендерим шаблон
+
+if __name__ == '__main__':
+    app.run()
